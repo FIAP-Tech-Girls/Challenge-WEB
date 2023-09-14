3c4fe12987da516f0233587262f36ff74206ee99
Luanacabezaolias <lu.cabezaolias@gmail.com>
2023-09-14 17:40:41 -0300

    arrumando a home

--- src/Duvidas.jsx
+++ src/Duvidas.jsx
@@ -3,7 +3,7 @@ import {} from 'react'
 function Duvidas() {
     return (
       <>
-      
+      <h1></h1>
       </>
     )
   }
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,9 @@ import {} from 'react'
 function Home() {
     return (
       <>
-      
+      <h1>Home</h1>
+      <h1>TechGirls</h1>
+      <h1></h1>
       </>
     )
   }
