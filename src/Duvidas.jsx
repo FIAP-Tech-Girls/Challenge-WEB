8f809b6823379dc68e0e12e6096cdfc941425dd3
anny-dias <logonrmlocal@fiap.com.br>
2023-09-15 11:29:27 -0300

    subindo página duvidas frequentes

--- src/Duvidas.jsx
+++ src/Duvidas.jsx
@@ -2,8 +2,66 @@ import {} from 'react'
 
 function Duvidas() {
     return (
-      <>
-      </>
+    <>
+        <div className="duvidasFrequentes">
+          <h1>Clique na pergunta desejada que irá aparecer a resposta!</h1>
+          <div className="perguntas">
+            <label className="pergunta">
+                Pergunta
+            </label>
+            <div className="resposta">
+                <p>Resposta</p>
+            </div>
+        </div>
+        <div className="pergunta">
+            <label className="pergunta">
+                Pergunta
+            </label>
+            <div className="resposta">
+                <p>Resposta</p>
+            </div>
+        </div>
+        <div className="pergunta">
+            <label className="pergunta">
+                Pergunta
+            </label>
+            <div className="resposta">
+                <p>Resposta</p>
+            </div>
+        </div>
+        <div className="pergunta">
+            <label className="pergunta">
+                Pergunta
+            </label>
+            <div className="resposta">
+                <p>Resposta</p>
+            </div>
+        </div>
+        <div className="pergunta">
+            <label className="pergunta">
+                Pergunta
+            </label>
+            <div className="resposta">
+                <p>Resposta</p>
+            </div>
+        </div>
+
+       <p id="tituloForms">
+            Se sua dúvida não foi respondida dentre essas, envie um formulário para as Tech Girls!
+            Responderemos em breve!
+        </p>
+
+        <div className="formulario">
+            <form name="formularioDuvidas">
+                <h3>FORMULÁRIO</h3>
+                    <label className="name">Nome: </label> 
+                    <label className="email">Email: </label>  
+                    <label className="assunto">Assunto: </label> 
+                    <label className="duvida">Deixe sua dúvida</label>
+              </form>
+        </div>
+      </div>
+    </>
     )
   }
   
--- src/Projetos.jsx
+++ src/Projetos.jsx
@@ -15,7 +15,7 @@ function Projetos() {
       <div className='conteudo-projetos-SFOME-texto'>
         <h2 className='titulo-projetos-SFOME'>Um projeto para um futuro melhor</h2>
         <p className='texto-projetos-SFOME'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eros tellus, congue vitae ultricies quis, imperdiet a sapien. Nam elit dolor, laoreet vel dolor vitae, consequat porttitor lectus. Proin cursus imperdiet neque id lobortis. Nullam cursus ligula sit amet enim elementum sagittis eget sed tellus. Integer augue libero, molestie in condimentum non, rhoncus ac lorem. Integer sagittis luctus dui. Quisque non sem lectus. Proin molestie ultricies lorem ac efficitur. Vestibulum eu diam id nisi aliquam convallis a in metus. Vivamus imperdiet odio eget elit mattis, ut dictum lacus pharetra. Etiam ultricies condimentum.</p>
-        <p className='frase-SFOME'>"A única forma de chegar ao impossível é acreditar que é possível"</p>
+        <p className='frase-SFOME'>A única forma de chegar ao impossível é acreditar que é possível</p>
       </div>
       </section>
      
