08c1d90353bdf8d946d73413151b9e9f8688c960
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:37:36 -0300

    subindo pagina techgirls

--- src/Techgirls.jsx
+++ src/Techgirls.jsx
@@ -3,7 +3,19 @@ import {} from 'react'
 function Techgirls() {
     return (
       <>
+      <section className='conteudo-techgirls'>
+      <h2 className='titulo-tech'>Quem nós somos?</h2>
+      <div className='conteudo-tech-imagem'>
+          <p className='texto-tech'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eros tellus, congue vitae ultricies quis, imperdiet a sapien. Nam elit dolor, laoreet vel dolor vitae, consequat porttitor lectus. Proin cursus imperdiet neque id lobortis. Nullam cursus ligula sit amet enim elementum sagittis eget sed tellus. Integer augue libero, molestie in condimentum non, rhoncus ac lorem. Integer sagittis luctus dui. Quisque non sem lectus. Proin molestie ultricies lorem ac efficitur. Vestibulum eu diam id nisi aliquam convallis a in metus. Vivamus imperdiet odio eget elit mattis, ut dictum lacus pharetra. Etiam ultricies condimentum.</p>
+      </div>
+      <div className='conteudo-tech-imagem'>
+          <p className='texto-tech'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eros tellus, congue vitae ultricies quis, imperdiet a sapien. Nam elit dolor, laoreet vel dolor vitae, consequat porttitor lectus. Proin cursus imperdiet neque id lobortis. Nullam cursus ligula sit amet enim elementum sagittis eget sed tellus. Integer augue libero, molestie in condimentum non, rhoncus ac lorem. Integer sagittis luctus dui. Quisque non sem lectus. Proin molestie ultricies lorem ac efficitur. Vestibulum eu diam id nisi aliquam convallis a in metus. Vivamus imperdiet odio eget elit mattis, ut dictum lacus pharetra. Etiam ultricies condimentum.</p>
+      </div>
       
+
+
+
+      </section>
       
       </>
     )
