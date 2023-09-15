533f6f6b3543ccb2d90a125467e35cfbd1ea9cb3
anny-dias <logonrmlocal@fiap.com.br>
2023-09-15 11:19:03 -0300

    subindo página tiana

--- src/Tiana.jsx
+++ src/Tiana.jsx
@@ -3,6 +3,21 @@ import {} from 'react'
 function Tiana() {
     return (
       <>
+      <section className='conteudo-tiana'>
+      <h2 className='titulo-tiana'>A nossa inteligência artificial</h2>
+      <div className='conteudo-tiana-imagem'>
+          <p className='texto-tiana'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eros tellus, congue vitae ultricies quis, imperdiet a sapien. Nam elit dolor, laoreet vel dolor vitae, consequat porttitor lectus. Proin cursus imperdiet neque id lobortis. Nullam cursus ligula sit amet enim elementum sagittis eget sed tellus. Integer augue libero, molestie in condimentum non, rhoncus ac lorem. Integer sagittis luctus dui. Quisque non sem lectus. Proin molestie ultricies lorem ac efficitur. Vestibulum eu diam id nisi aliquam convallis a in metus. Vivamus imperdiet odio eget elit mattis, ut dictum lacus pharetra. Etiam ultricies condimentum.</p>
+      </div>
+      <div className='conteudo-tiana-imagem'>
+          <p className='texto-tiana'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eros tellus, congue vitae ultricies quis, imperdiet a sapien. Nam elit dolor, laoreet vel dolor vitae, consequat porttitor lectus. Proin cursus imperdiet neque id lobortis. Nullam cursus ligula sit amet enim elementum sagittis eget sed tellus. Integer augue libero, molestie in condimentum non, rhoncus ac lorem. Integer sagittis luctus dui. Quisque non sem lectus. Proin molestie ultricies lorem ac efficitur. Vestibulum eu diam id nisi aliquam convallis a in metus. Vivamus imperdiet odio eget elit mattis, ut dictum lacus pharetra. Etiam ultricies condimentum.</p>
+      </div>
+      <div className='conteudo-tiana-diferencial'>
+        <h2 className='texto-tiana-diferencial'>Nossos diferenciais</h2>
+      </div>
+      <div className='conteudo-tiana-solucao'>
+        <h2 className='texto-tiana-solucao'>Para integrar na nossa solução</h2>
+      </div>
+      </section>
      
       </>
     )
